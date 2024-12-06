51c4abb964f663c8fe6d586d86378bd5f99c6c76
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-12-07 00:45:59 +0530

    tailwind css setup

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
 	console.log('What am i doing here?');
 	return (
 		<>
+			<h1 className="h1-bold">Welcome to the world of Next.js</h1>
 			<div className="text-3xl font-black">Hello World</div>
 		</>
 	);
