5eedc906592f1c1d25145796e7ecbd84d1f318ec
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-12-07 00:54:59 +0530

    Added fonts

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ export default function Home() {
 	return (
 		<>
 			<h1 className="h1-bold">Welcome to the world of Next.js</h1>
+			<h1 className="h1-bold font-space-grotesk">
+				Welcome to the world of Next.js
+			</h1>
+
 			<div className="text-3xl font-black">Hello World</div>
 		</>
 	);
