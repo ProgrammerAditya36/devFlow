9fb5baaee1a8c766fed351619bf40c7de89e1792
Aditya Amit Kinjawadekar <programmeraditya36@gmail.com>
2024-12-07 14:17:24 +0530

    Basic Navbar set

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+import { ThemeProvider } from 'next-themes';
 import React from 'react';
+
 import './globals.css';
+import Navbar from '@/components/navigation/navbar';
 
 const inter = localFont({
 	src: './fonts/Inter.ttf',
@@ -26,11 +29,19 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
 			<body
 				className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
 			>
-				{children}
+				<ThemeProvider
+					attribute={'class'}
+					defaultTheme="dark"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
 export default function Home() {
-	console.log('What am i doing here?');
-	return (
-		<>
-			<h1 className="h1-bold">Welcome to the world of Next.js</h1>
-			<h1 className="h1-bold font-space-grotesk">
-				Welcome to the world of Next.js
-			</h1>
-
-			<div className="text-3xl font-black">Hello World</div>
-		</>
-	);
+	return <></>;
 }
